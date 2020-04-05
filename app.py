90e9461e3d010d3258c627f8f198d42a0a997895
kenessajr <jeanremy1806@gmail.com>
2020-04-05 12:16:10 +0200

    app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+"""
+TODO:
+- Analyse the internet speed based on time (Location).
+- Analysis based on average speed per telco (Location)
+- Sort best telcos on average speed collected (Location)
+- List poorest networks to avoid (Location)
+- Compare 3G over 4G average speed (Location)
+- Compare 3G networks providers (Location)
+- Compare 4G networks providers (Location)
+- Recommend the best network for working at home (Location).
+- Recommend the best 4G network for mobile network (Location).
+- Recommend the best 3G network to use according to location (Location)
+"""
